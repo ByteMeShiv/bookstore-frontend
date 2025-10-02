51622fdd5b995058562b8906cb0b55197ed16c3e
TheSarcasticOne69 <shivamkushwaha197@gmail.com>
2025-10-02 18:30:12 +0530

    Ready for Vercel deployment

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 // src/api.js
-const BASE_URL = "https://bookstore-api-tp5w.onrender.com/books";
+const BASE_URL = process.env.REACT_APP_API_URL;
+export default BASE_URL;
 
 export const getBooks = async () => {
   const response = await fetch(BASE_URL);
